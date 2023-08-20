3e6e17303ca4cd51d51bd5fbb39ca3db87b9c1bb
Mia Dinh <miadinh38@gmail.com>
2023-08-20 04:24:16 -0400

    Refactor modularity and add test using mocha & chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,25 @@
 const request = require("request");
 
-const agrs = process.argv.slice(2);
-const breedName = agrs[0];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error: ',error);
-  } else if (response.statusCode !== 200) {
-    console.error('Status: ', response.statusCode);
-  } else {
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(`Error: ${error}`, null);
+    }
+    if (response.statusCode !== 200) {
+      callback(`Status: ${response.statusCode}`, null);
+    }
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log('Breed Not Found: ', breedName);
+      callback(`Breed Not Found: ${breedName}`, null);
     } else {
       const firstEntry = data[0];
-      console.log(firstEntry.description);
+      callback(null, firstEntry.description);
     }
-  }
-});
+  });
+};
 
+
+module.exports = { fetchBreedDescription };
